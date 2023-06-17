0185434d87780dfc9c31e96c8ab841cee83b00bb
Andrew Rush <inarush0@gmail.com>
2023-06-17 12:36:09 -0400

    added prettier config for tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,5 +5,5 @@
 	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
-<h1 class="text-3xl font-bold underline text-2xl">Welcome to SvelteKit</h1>
+<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
